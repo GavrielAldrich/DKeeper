cfd365035971141d94c89d05ac325c04480305ce
GavrielAldrich <gavrielaldrixh@gmail.com>
2024-04-14 18:35:07 +0700

    0.1 Testing

---
+++ src/dkeeper_backend/main.mo
@@ -0,0 +1,7 @@
+actor DKeeper{
+  public func test(){
+    // Debug.print(debug_show(myStableObject.x));
+    // myStableObject.x := "confirmed";
+    // Debug.print(debug_show(myStableObject.x));
+  };
+};
---
+++ src/dkeeper_frontend/src/App.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { dkeeper_backend } from "declarations/dkeeper_backend"
+import Header from "./components/Header"
+import Note from "./components/Note"
+import CreateArea from "./components/CreateArea"
+import Footer from "./components/Footer"
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  async function checkConnect(){
+    await dkeeper_backend.test();
+  }
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={
+        // addNote
+        checkConnect
+        } />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
